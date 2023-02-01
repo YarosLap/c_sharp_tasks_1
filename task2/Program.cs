d4569440346b9a1313e3f278e8aa8b07209cdde2
Yaros Lap <yaroslap86@gmail.com>
2023-02-01 16:28:36 +0300

    initial commit

---
+++ task2/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+if (a > b)
+    Console.WriteLine($"Большее число {a}, меньшее число {b}");
+else if (a < b)
+    Console.WriteLine($"Большее число {b}, меньшее число {a}");
+else if (a == b)
+    Console.WriteLine("Числа равны");
+else Console.WriteLine("Ошибка ввода");
---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+
+if (b > a)
+    max = b;
+if (c > b)
+    max = c;
+
+Console.WriteLine($"Максимальное число {max}");
